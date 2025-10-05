1177d284429010a4b4259bf235740416a4ec1e9e
LiJuncongDIKU <ljc1350220@163.com>
2025-10-05 06:23:42 +0000

    Deploying to gh-pages from  @ 8e3e593d77a512dfa1fcd5efc8c3e1b120b37057 🚀

---
+++ assets/client.api-DUxc9zuG.js
@@ -0,0 +1,84 @@
+const n=`import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios';
+import { ElMessageBox } from 'element-plus';
+
+declare module 'axios' {
+  interface AxiosInstance {
+    defineApi: (uri: string, options?: AxiosRequestConfig) => Function;
+  }
+}
+function defineApi(this: AxiosInstance, uri: string, options: AxiosRequestConfig = {}) {
+  return (data: any, config: AxiosRequestConfig = {}) => {
+    const exOption = {
+      method: 'POST', // 默认post
+      ...options, // 定义api时候的配置
+      ...config, // 调用时候的配置, 优先级最高
+    };
+    if (exOption.method.toLocaleLowerCase() === 'post') {
+      exOption.data = data;
+    } else {
+      exOption.params = data;
+    }
+    console.log('exOption', exOption)
+    return this.request({ // 定义实例时已带有默认配置
+      url: uri,
+      ...exOption,
+    }).catch((e) => {
+      ElMessageBox({
+        title: e.code || '提示',
+        message: '展示一下统一异常处理的弹窗:' + e.message
+      })
+      throw '禁止后续的then执行'
+    })
+  }
+}
+
+
+function createAxios(uri: string, options: AxiosRequestConfig = {}): AxiosInstance {
+  const instance: AxiosInstance = axios.create({
+    baseURL: uri,
+    ...options
+  });
+  // 配合声明使用，创建实例时候，就必须插入defineApi方法
+  instance.defineApi = defineApi;
+  return instance;
+}
+export const baseHost = import.meta.env.MODE === "development" ? 'http://localhost:5173' : 'http://www.xxx.com';
+export const adHost = import.meta.env.MODE === "development" ? 'http://localhost:5173' : 'http://ad.xxx.com';
+// 比如说，我们后端对于用户服务、广告服务等是分开管理的，甚至需要不同的过滤器、甚至域名、开发时代理
+export const adAxios = createAxios(\`\${adHost}/api/ad\`);
+export const authAxios = createAxios(\`\${baseHost}/api/auth\`);
+export const publicAxios = createAxios(\`\${baseHost}/api/\`); // 不需要登录的接口
+
+function useAuthInterceptor(axiosInstance: AxiosInstance) {
+  axiosInstance.interceptors.request.use((config) => {
+    // 可以在这里添加一些公共的请求头
+    config.headers['AccessToken'] = 'xxxToken';
+    config.headers['X-Custom-Header'] = 'SomeValue';
+    return config;
+  });
+}
+
+useAuthInterceptor(authAxios);
+useAuthInterceptor(adAxios);
+// useAuthInterceptor(publicAxios); 这些接口不需要登录许可
+
+function useErrorInterceptor(axiosInstance: AxiosInstance) {
+  // 统一处理异常返回
+  axiosInstance.interceptors.response.use((res) => {
+    return res;
+  }, (err) => {
+    // 统一处理异常返回
+    console.error(err);
+    return Promise.reject(err);
+  });
+}
+
+useErrorInterceptor(authAxios);
+useErrorInterceptor(adAxios);
+useErrorInterceptor(publicAxios);`,e=`import { authAxios, publicAxios } from './request'
+
+export default {
+  getLoginInfo: authAxios.defineApi('/getLoginInfo'),
+  getVerifyCode: publicAxios.defineApi('/getVerifyCode'),
+  login: publicAxios.defineApi('/login'),
+}`;export{e as c,n as r};
